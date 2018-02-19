d7b08702c90162e5c50f40c8b2fbc970abaf78ce
Daniel Supernault <danielsupernault@gmail.com>
2018-02-18 21:11:40 -0700

    Add username and email_verified_at fields to user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username', 39)->unique()->index();
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
