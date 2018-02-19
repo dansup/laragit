a7e5e6d33ce71fae690df8b6c9dc67e53c0ee356
Daniel Supernault <danielsupernault@gmail.com>
2018-02-18 23:46:31 -0700

    Add Font Awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
@@ -59,7 +60,7 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="">
             @yield('content')
         </main>
     </div>
