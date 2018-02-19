681ca2ddcbe752c19ccf59e5822d7cfcd6bf1e85
Daniel Supernault <danielsupernault@gmail.com>
2018-02-18 21:43:18 -0700

    Add GitWrapper Library

--- config/app.php
+++ config/app.php
@@ -209,6 +209,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Grit' => \GitWrapper\GitWrapper::class,
+
     ],
 
 ];
