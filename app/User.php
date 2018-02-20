a64d51f57773a9b2155e6bc7b3142f9ed1e0136d
Daniel Supernault <danielsupernault@gmail.com>
2018-02-19 20:27:10 -0700

    Add repository methods to User model

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Repository;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function repositories()
+    {
+        return $this->hasMany(Repository::class);
+    }
+
+    public function repo($name = false)
+    {
+        if($name == false) { return; }
+
+        return $this->repositories()->whereSlug($name)->first();
+    }
 }
