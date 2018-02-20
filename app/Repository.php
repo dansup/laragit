3a417ef73c04183127ed932981b101771966176a
Daniel Supernault <danielsupernault@gmail.com>
2018-02-19 20:09:12 -0700

    Add new methods to Repository model
    Added:
    - url() - for generating a url of the repo
    - grit() - for bootstrapping a git working copy of the repo
    - branches() - returns an array of branches

--- app/Repository.php
+++ app/Repository.php
@@ -3,8 +3,33 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Grit;
 
 class Repository extends Model
 {
-    //
+    public function url()
+    {
+      return url($this->slug);
+    }
+    
+    public function grit()
+    {
+      $git = new Grit('/usr/bin/git');
+      $repo = $git->workingCopy($this->path);
+      return $repo;
+    }
+
+    public function branches()
+    {
+      $grit = $this->grit();
+      $output = $grit->branch(['a'=>true]);
+      $branches = preg_split("/\r\n|\n|\r/", rtrim($output));
+      return array_map([$this, 'trimBranch'], $branches);
+    }
+
+
+    public function trimBranch(string $branch): string
+    {
+        return ltrim($branch, ' *');
+    }
 }
