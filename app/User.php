02f0bdfa6a0f043a8148342853869349c3a9104e
Daniel Supernault <danielsupernault@gmail.com>
2019-01-16 21:01:16 -0700

    Update User model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'username',
     ];
 
     /**
@@ -27,6 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function url()
+    {
+        return url($this->username);
+    }
 
     public function repositories()
     {
