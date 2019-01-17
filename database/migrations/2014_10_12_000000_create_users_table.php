9db2c2fca6379fb93b9bd220e3a9ad864fcd1cfe
Daniel Supernault <danielsupernault@gmail.com>
2019-01-16 20:58:30 -0700

    Add public svgs

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name');
             $table->string('username', 39)->unique()->index();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('role')->default('user');
             $table->rememberToken();
+            $table->boolean('2fa_enabled')->default(false);
+            $table->string('2fa_secret')->nullable();
+            $table->json('2fa_backup_codes')->nullable();
+            $table->timestamp('2fa_setup_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
